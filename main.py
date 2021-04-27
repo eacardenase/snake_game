00b12061aa6eb6d0acc7ed77049cb7ae74c61f3f
Edwin Cardenas <>
2021-04-26 21:57:41 -0500

    Basic setup

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame
+import  time
+
+if __name__ == "__main__":
+    pygame.init()
+
+    surface = pygame.display.set_mode((1000, 500))
+    surface.fill((255, 255, 255))
+    pygame.display.update()
+    time.sleep(5)
